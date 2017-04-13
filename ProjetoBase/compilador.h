23e66f97e9532a9b8ed2d1ceb67c40ed3853024c
chjonCodes <jonathanbrasil10@hotmail.com>
2017-04-12 22:25:14 -0300

    all basic identifiers added
    Signed-off-by: chjonCodes <jonathanbrasil10@hotmail.com>

--- ProjetoBase/compilador.h
+++ ProjetoBase/compilador.h
@@ -18,6 +18,12 @@ typedef enum simbolos {
   simb_identificador, simb_numero,
   simb_ponto, simb_virgula, simb_ponto_e_virgula, simb_dois_pontos,
   simb_atribuicao, simb_abre_parenteses, simb_fecha_parenteses,
+  simb_identificador, simb_label, simb_tipo, simb_array,
+  simb_procedure, simb_function, simb_goto, simb_if, simb_then,
+  simb_else, simb_while, simb_do, simb_or, simb_and, simb_not,
+  simb_mais, simb_menos, simb_asterisco, simb_div, simb_igual,
+  simb_maior, simb_menor, simb_maior_igual, simb_menor_igual,
+  simb_diferente
 } simbolos;
 
 
